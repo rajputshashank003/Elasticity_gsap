8d1feffe8806bb6eee64c38df77f6294f60cf3c0
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2024-12-30 23:00:38 +0530

    fix bugs in header and make animation of alphabet faster

--- src/components/StartupAnimation.jsx
+++ src/components/StartupAnimation.jsx
@@ -1,17 +1,18 @@
 import { useEffect } from 'react';
 import gsap from 'gsap';
 import { TextPlugin } from 'gsap/TextPlugin';
+import { useGSAP } from '@gsap/react';
 
 gsap.registerPlugin(TextPlugin); 
 
 const StartupAniation = () => {
-    useEffect(() => {
+    useGSAP(() => {
         const textArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
         const tl = gsap.timeline();
-        textArray.forEach((text, index) => {
+        textArray.forEach((text) => {
             tl.to(".text-span", {
                 text: text,          
-                duration: 0.07,         
+                duration: 0.05,         
                 stagger: 0.07,
                 ease : "power3.inOut",
             });
